13f4b6caab870af73fa8667a5ebcf0ddd14029e1
Takagi Kensuke <janus.wel.3@gmail.com>
2019-01-02 17:02:06 +0900

    :white_check_mark: add test case for function without arguments

--- src/deep-memoize.test.ts
+++ src/deep-memoize.test.ts
@@ -1,13 +1,20 @@
 import deepMemoize from './deep-memoize'
 
 describe('memoize', () => {
-  it('returns memoized function', () => {
-    const memoizedAdd = deepMemoize((a: number, b: number) => ({ answer: a + b }))
-    expect(typeof memoizedAdd).toBe('function')
-    const oneTwo = memoizedAdd(1, 2)
-    expect(memoizedAdd(1, 2)).toBe(oneTwo)
+  it('returns memoized function with no arguments', () => {
+    const nop = deepMemoize(() => ({}))
+    expect(typeof nop).toBe('function')
+    const empty = nop()
+    expect(nop()).toBe(empty)
+  })
 
-    const twoOne = memoizedAdd(2, 1)
+  it('returns memoized function with some arguments', () => {
+    const add = deepMemoize((a: number, b: number) => ({ answer: a + b }))
+    expect(typeof add).toBe('function')
+    const oneTwo = add(1, 2)
+    expect(add(1, 2)).toBe(oneTwo)
+
+    const twoOne = add(2, 1)
     expect(twoOne).toEqual(oneTwo)
     expect(twoOne).not.toBe(oneTwo)
   })
